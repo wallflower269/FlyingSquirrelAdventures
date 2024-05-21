ddf6cb708135e37ee989185697264a5f6991132c
mai968 <138955025+mai968@users.noreply.github.com>
2024-05-21 23:38:19 +0700

    mey

---
+++ src/GameState/Level2State.java
@@ -0,0 +1,227 @@
+package GameState;
+
+import Main.Game;
+import Main.GamePanel;
+import TileMap.*;
+import Entity.*;
+import Entity.Enemies.*;
+import Audio.AudioPlayer;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class Level2State extends GameState {
+	
+	private TileMap tileMap;
+	private Background bg;
+	
+	private Player player;
+	
+	private ArrayList<Enemy> enemies;
+	private ArrayList<Explosion> explosions;
+	
+	private HUD hud;
+
+	private Teleport teleport;
+	
+	private AudioPlayer bgMusic;
+	
+	public Level2State(GameStateManager gsm) {
+		this.gsm = gsm;
+		init();
+	}
+	
+	public void init() {
+		
+		tileMap = new TileMap(30);
+		tileMap.loadTiles("/Tilesets/grasstileset.gif");
+		tileMap.loadMap("/Maps/level2-2.map");
+		tileMap.setPosition(0, 0);
+		tileMap.setTween(1);
+		
+		bg = new Background("/Backgrounds/moonbg2.gif", 0.1);
+
+		player = new Player(tileMap , gsm) {
+			// @Override
+			// public boolean dead() {
+			// 	return false;
+			// }
+		};
+		player.setPosition(100, 100);
+		
+		populateEnemies();
+		
+		explosions = new ArrayList<Explosion>();
+		
+		hud = new HUD(player);
+		
+		bgMusic = new AudioPlayer("/Music/level1-1.mp3");
+		bgMusic.play();
+
+		//thêm code:
+		// Initialize and set the position of the teleport
+		teleport = new Teleport(tileMap);
+		// Khởi tạo Teleport với toạ độ mục tiêu và GameStateManager
+		teleport.setPosition(3127, 150);  // vị trí của Teleport
+		
+		//
+	
+		
+	}
+	private void checkPlayerStatus() {
+		if (player.isDead() == true) {  // Giả sử isDead() kiểm tra nếu trạng thái dead là true
+			gsm.setState(GameStateManager.GAMEOVERSTATE);
+			Game.stopMusic();
+		}
+		if (player.isAtPosition(3127, 166)) {
+			gsm.setState(GameStateManager.WINNERSTATE);     // chuyển trạng thái level2/Winner
+		}
+
+	}
+	
+	private void populateEnemies() {
+		
+		enemies = new ArrayList<Enemy>();
+		
+		Slugger s;
+		Point[] points = new Point[] {
+			new Point(200, 100),
+			new Point(860, 200),
+			new Point(1525, 200),
+			new Point(1680, 200),
+			new Point(1800, 200)
+		};
+		for(int i = 0; i < points.length; i++) {
+			s = new Slugger(tileMap);
+			s.setPosition(points[i].x, points[i].y);
+			enemies.add(s);
+		}
+		
+	}
+
+	public void update() {
+		// Update player
+		player.update();
+		teleport.update();
+
+		// Set tilemap position
+		tileMap.setPosition(
+				GamePanel.WIDTH / 2 - player.getx(),
+				GamePanel.HEIGHT / 2 - player.gety()
+		);
+
+		// Set background position
+		bg.setPosition(tileMap.getx(), tileMap.gety());
+
+		// Attack enemies
+		player.checkAttack(enemies);
+
+		// Check player status (e.g., health, dead)
+		checkPlayerStatus();
+
+		// Check if player has reached the end of the level
+		// checkForWin();
+
+		// Update all enemies
+		for (int i = 0; i < enemies.size(); i++) {
+			Enemy e = enemies.get(i);
+			e.update();
+			if (e.isDead()) {
+				enemies.remove(i);
+				i--;
+				explosions.add(new Explosion(e.getx(), e.gety()));
+			}
+		}
+
+		// Update explosions
+		for (int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).update();
+			if (explosions.get(i).shouldRemove()) {
+				explosions.remove(i);
+				i--;
+			}
+
+		}
+
+		// them code
+		if (teleport != null) {
+			teleport.update();
+		}
+
+
+		if (player.getHealth() <= 0) {
+			gsm.setState(GameStateManager.GAMEOVERSTATE); // chuyển trạng thái Game Over
+		}
+
+		//Thuc Minh: For level 2
+		// checkForWin();
+	}
+
+	
+	public void draw(Graphics2D g) {
+		
+		// draw bg
+		bg.draw(g);
+		
+		// draw tilemap
+		tileMap.draw(g);
+		
+		// draw player
+		player.draw(g);
+		
+		// draw enemies
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).draw(g);
+		}
+		
+		// draw explosions
+		for(int i = 0; i < explosions.size(); i++) {
+			explosions.get(i).setMapPosition(
+				(int)tileMap.getx(), (int)tileMap.gety());
+			explosions.get(i).draw(g);
+		}
+
+		// Teleport
+		if (teleport != null) {
+			teleport.draw(g);
+		}
+		
+		// draw hud
+		hud.draw(g);
+		
+	}
+	
+	public void keyPressed(int k) {
+		if(k == KeyEvent.VK_LEFT) player.setLeft(true);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(true);
+		if(k == KeyEvent.VK_UP) player.setUp(true);
+		if(k == KeyEvent.VK_DOWN) player.setDown(true);
+		if(k == KeyEvent.VK_W) player.setJumping(true);
+		if(k == KeyEvent.VK_E) player.setGliding(true);
+		if(k == KeyEvent.VK_R) player.setScratching();
+		if(k == KeyEvent.VK_F) player.setFiring();
+	}
+	
+	public void keyReleased(int k) {
+		if(k == KeyEvent.VK_LEFT) player.setLeft(false);
+		if(k == KeyEvent.VK_RIGHT) player.setRight(false);
+		if(k == KeyEvent.VK_UP) player.setUp(false);
+		if(k == KeyEvent.VK_DOWN) player.setDown(false);
+		if(k == KeyEvent.VK_W) player.setJumping(false);
+		if(k == KeyEvent.VK_E) player.setGliding(false);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
